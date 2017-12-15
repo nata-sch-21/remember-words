58b03b5ad59b90ffd8ad6668f4893cbf0faf636a
Natalya Shostak <natalya.shostak@tulip.io>
2017-12-15 13:03:35 +0300

    Added css loader to prod

--- client/webpack.prod.config.js
+++ client/webpack.prod.config.js
@@ -56,6 +56,18 @@ const config = {
           ],
         }),
       },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]',
+            },
+          },
+        ],
+      },
     ],
   },
   node: {
